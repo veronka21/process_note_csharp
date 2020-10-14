137a80347d9223752448ade1e06cf697b703d4dd
Veronika Takács <roni9764@gmail.com>
2020-10-14 16:22:28 +0200

    Create ThreadWindowViewModel

---
+++ ProcessNote/ProcessNote/ViewModels/ThreadWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using ProcessNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessNote.ViewModels
+{
+    public class ThreadWindowViewModel : ViewModelBase
+    {
+        ProcessThreadCollection _receivedThreadCollection;
+        internal ObservableCollection<Thread> Threads { get; set; } = new ObservableCollection<Thread>();
+
+        public ThreadWindowViewModel(ProcessThreadCollection ThreadCollection) 
+        {
+            _receivedThreadCollection = ThreadCollection;
+            Seed();
+        }
+
+        private void Seed()
+        {
+            foreach (ProcessThread thread in _receivedThreadCollection) {
+                Thread currentThread = new Thread(thread.Id);
+                try {
+                    currentThread.BasePriority = thread.BasePriority.ToString();
+                    currentThread.PriorityLevel = thread.PriorityLevel.ToString();
+                } catch (Exception) {
+                }
+                Threads.Add(currentThread);
+            }
+        }
+    }
+}
